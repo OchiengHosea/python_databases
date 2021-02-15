b45ed38d9da6e49fad2f6f5d2626ba9ad0d3b7f0
@Ochieng__ <jrhosea8@gmail.com>
2021-02-15 23:51:04 +0300

    Python PostgresSQL Intro

---
+++ pypg.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect(database="rough_db", user="postgres", password="postgres", host="localhost", port="5432")
+
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE Sales (ORDER_NUM INT PRIMARY KEY,
+                    ORDER_TYPE TEXT,
+                    CUST_NAME TEXT,
+                    PROD_NUMBER TEXT,
+                    PROD_NAME TEXT,
+                    QUANTITY INT,
+                    PRICE REAL,
+                    DISCOUNT REAL,
+                    ORDER_TOTAL REAL
+                );''')
+
+conn.commit()
+conn.close()
